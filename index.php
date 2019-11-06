0ca75b5ee4b77e224f1da7d82d956f18e4917e98
Martinus95M <martinus.mortensen@gmail.com>
2019-11-06 13:49:28 +0100

    Frontpage

--- index.php
+++ index.php
@@ -9,13 +9,12 @@
                 while( have_posts() ): the_post(); ?>
 
                         <p><?php the_content(); ?></p>
-                        <div class="scroll"></div>
 
                 <?php endwhile;
 		
 	endif;
 			
-	?>
+        ?>
         </section>
 </main>
 
