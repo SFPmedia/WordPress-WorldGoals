924ce466954acf3e8f37869b3b456d52d0a4a7c2
SFPmedia <s-pauck@hotmail.com>
2019-11-07 15:19:10 +0100

    Opsætning af konto-siden

---
+++ page-konto.php
@@ -0,0 +1,21 @@
+<?php get_header();?>
+
+<main class="grid">
+        <section class="content-grid colors konto-oversigt">
+        <?php 
+	
+	if( have_posts() ):
+		
+                while( have_posts() ): the_post(); ?>
+
+                        <p><?php the_content(); ?></p>
+
+                <?php endwhile;
+		
+	endif;
+			
+        ?>
+        </section>
+</main>
+
+<?php get_footer();?>
